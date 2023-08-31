8406e3fca33ebfe50dc2f7eeb9b4b1574f8a7e1e
Fertilesoil <gasnbloop@gmail.com>
2023-08-31 12:08:04 -0300

    Model Conta

--- Conta Bancária/Program.cs
+++ Conta Bancária/Program.cs
@@ -1,52 +1,67 @@
-﻿namespace Conta_Bancária
-{
-    internal class Program 
-    {
+﻿using Conta_Bancária.Model;
 
-        static int Menu(ref int opcao)
-        {
-            
-            Console.BackgroundColor = ConsoleColor.Black;
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.Clear();
-            Console.WriteLine("                                                                                       ");
-            Console.WriteLine("                                                                                       ");
-            Console.WriteLine("                                                                                       ");
-            Console.WriteLine("                           %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
-            Console.WriteLine("                           $                                                          $");
-            Console.WriteLine("                           $     *****            Banco Dawn Yarn           *****     $");
-            Console.WriteLine("                           $                                                          $");
-            Console.WriteLine("                           %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
-            Console.WriteLine("                           $                                                          $");
-            Console.WriteLine("                           $             1   Criar Conta                              $");
-            Console.WriteLine("                           $             2   Listar Todas as Contas                   $");
-            Console.WriteLine("                           $             3   Buscar Conta por Número                  $");
-            Console.WriteLine("                           $             4   Atualizar Dados da Conta                 $");
-            Console.WriteLine("                           $             5   Apagar Conta                             $");
-            Console.WriteLine("                           $             6   Sacar                                    $");
-            Console.WriteLine("                           $             7   Depositar                                $");
-            Console.WriteLine("                           $             8   Transferir Valores Entre Contas          $");
-            Console.WriteLine("                           $             9   Sair                                     $");
-            Console.WriteLine("                           $                                                          $");
-            Console.WriteLine("                           %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
-            Console.WriteLine("                           %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
-            Console.WriteLine("                           $                                                          $");
-            Console.WriteLine("                           $          Informe a Operação Que Deseja Realizar          $");
-            Console.WriteLine("                           $                                                          $");
-            Console.WriteLine("                           %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
-            opcao = Convert.ToInt32(Console.ReadLine());
-            Console.Clear();
-            return opcao;
-        }
+namespace Conta_Bancária
+{
+    internal class Program
+    {
 
         static void Main(string[] args)
         {
             int opcao = 0;
             string sn;
 
+            Conta conta1 = new Conta(1, 123, 1, "Galhardo", 1000000.00M);
+
+            conta1.Visualizar();
+            conta1.SetNumero(345);
+            Console.WriteLine(conta1.GetNumero());
+
+            conta1.Sacar(1000);
+
+            conta1.Visualizar();
+
+            conta1.Depositar(5000); 
+            
+            conta1.Visualizar();
+
+        
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            inicio:
             while (opcao != 9)
             {
-                opcao = Menu(ref opcao);
+            
+                Console.WriteLine("                                                                                       ");
+                Console.WriteLine("                                                                                       ");
+                Console.WriteLine("                                                                                       ");
+                Console.WriteLine("                           %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+                Console.WriteLine("                           $                                                          $");
+                Console.WriteLine("                           $     *****            Banco Dawn Yarn           *****     $");
+                Console.WriteLine("                           $                                                          $");
+                Console.WriteLine("                           %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+                Console.WriteLine("                           $                                                          $");
+                Console.WriteLine("                           $             1   Criar Conta                              $");
+                Console.WriteLine("                           $             2   Listar Todas as Contas                   $");
+                Console.WriteLine("                           $             3   Buscar Conta por Número                  $");
+                Console.WriteLine("                           $             4   Atualizar Dados da Conta                 $");
+                Console.WriteLine("                           $             5   Apagar Conta                             $");
+                Console.WriteLine("                           $             6   Sacar                                    $");
+                Console.WriteLine("                           $             7   Depositar                                $");
+                Console.WriteLine("                           $             8   Transferir Valores Entre Contas          $");
+                Console.WriteLine("                           $             9   Sair                                     $");
+                Console.WriteLine("                           $                                                          $");
+                Console.WriteLine("                           %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+                Console.WriteLine("                           %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+                Console.WriteLine("                           $                                                          $");
+                Console.WriteLine("                           $          Informe a Operação Que Deseja Realizar          $");
+                Console.WriteLine("                           $                                                          $");
+                Console.WriteLine("                           %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+                opcao = Convert.ToInt32(Console.ReadLine());
+                
+            }
+            
+            
+            
                 switch (opcao)
                 {
                     case 1:
@@ -95,7 +110,7 @@
 
                         if (sn.Equals("nao") || sn.Equals("não") || sn.Equals("n"))
                         {
-                            Menu(ref opcao);
+                         goto inicio;
                         }
                         else
                         {
@@ -137,7 +152,7 @@
                         Console.ReadKey();
                         break;
                 }
-            }
+            
 
 
 
